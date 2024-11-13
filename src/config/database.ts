b9c345bbc2f4d37869cee5e9fe4d52ef1c210a22
Ruslana Gnatenko <rgnatenko22@gmail.com>
2024-11-13 17:06:26 +0200

    .

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
-
-mongoose.connect(MONGO_URI)
-.then(() => console.log('Database connected'))
-.catch(err => console.error('Database connection error', err));
--- src/config/firebase.ts
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin';
-import path from 'path';
-
-admin.initializeApp({
-credential: admin.credential.cert(path.resolve(__dirname, '../../firebase-adminsdk.json')),
-});
-
-export default admin;
